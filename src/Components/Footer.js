cc0cf7e1457fa5646c36da0ecc775e0f046d2ef5
Maciej Kasperczyk <makasperczyk1@gmail.com>
2022-03-03 21:19:20 +0100

    Add files via upload

---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return <h1 className="author"> Maciej Kasperczyk 2022 </h1>
+}
+
+
+
+
+
+export default Footer;
+
+
+
